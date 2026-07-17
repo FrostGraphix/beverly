ebad607bbb14f880d9ce478d8be972e2192bb646
FrostGraphix <124405987+FrostGraphix@users.noreply.github.com>
2026-07-17 11:56:22 +0100

    Remove temporary artifacts and login framing (#19)
    * Remove temporary repository artifacts
    
    * Remove login page framing
    
    ---------
    
    Co-authored-by: FrostGraphix <FrostGraphix@users.noreply.github.com>

--- check_swagger.js
+++
@@ -1,1 +0,0 @@
-const fs=require('fs'); const s=JSON.parse(fs.readFileSync('tmp/swagger.json','utf8')); console.log(JSON.stringify(s.components.schemas['DailyDataMeterReadRequest'].properties, null, 2));
